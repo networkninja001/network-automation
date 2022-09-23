96a2c1b4f473bcbf2094ffc1508e9ee24c5b671d
Patrick <networkninja.pr@gmail.com>
2022-09-23 13:38:13 -0400

    Testing connectiontest-fortinet

--- connectiontest-fortinet.py
+++ connectiontest-fortinet.py
@@ -1,4 +1,5 @@
 #! /usr/bin/python3
+
 from netmiko import ConnectHandler
 from netmiko.ssh_exception import  NetMikoTimeoutException
 from paramiko.ssh_exception import SSHException 
@@ -19,36 +20,36 @@ with open('devices.txt') as firewalls:
 			'password': password,
 }
 
-    Timeouts=open("Connection time outs.txt", "a")
-    Authfailure=open("Auth failures.txt", "a")
-    SSHException=("SSH Failure.txt", 'a')
-    EOFError=("EOFerrors.txt",'a')
-    UnknownError=("UnknownError.txt",'a')
+		Timeouts=open("Connection time outs.txt", "a")
+		Authfailure=open("Auth failures.txt", "a")
+		SSHException=("SSH Failure.txt", 'a')
+		EOFError=("EOFerrors.txt",'a')
+		UnknownError=("UnknownError.txt",'a')
     
-    try:
-        net_connect = ConnectHandler(**firewall)
-        print('Connecting to ' + IP)
+	try:
+		net_connect = ConnectHandler(**firewall)
+		print('Connecting to ' + IP)
 		print('-'*79)
 		net_connect.enable()
 		print(net_connect.find_prompt())  
-        output=net_connect.send_command('get system status')
-        print(output)
-    except (AuthenticationException):
-        print ('Authentication Failure: ' + IP)
-        Authfailure.write('\n' + IP)
-        continue 
-    except (NetMikoTimeoutException):
-        print ('\n' + 'Timeout to device: ' + IP)
-        Timeouts.write('\n' + IP)
-        continue
-    except (SSHException):
-        print ('SSH might not be enabled: ' + IP)
-        SSHException.write('\n' + IP)
-        continue 
-    except (EOFError):
-        print ('\n' + 'End of attempting device: '  IP)
-        EOFError.write('\n' + IP)
-        continue
-    except unknown_error:
-        print ('Some other error: ' + str(unknown_error))
-        continue
+		output=net_connect.send_command('get system status')
+		print(output)
+	except (AuthenticationException):
+		print ('Authentication Failure: ' + IP)
+		Authfailure.write('\n' + IP)
+		continue 
+	except (NetMikoTimeoutException):
+		print ('\n' + 'Timeout to device: ' + IP)
+		Timeouts.write('\n' + IP)
+		continue
+	except (SSHException):
+		print ('SSH might not be enabled: ' + IP)
+		SSHException.write('\n' + IP)
+		continue 
+	except (EOFError):
+		print ('\n' + 'End of attempting device: ' + IP)
+		EOFError.write('\n' + IP)
+		continue
+	except unknown_error:
+		print ('Some other error: ' + str(unknown_error))
+		continue
